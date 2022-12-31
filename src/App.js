dcc42b6e6e092a0e2d9f7830819bb06df39fdc90
hafeezrq <hafeezrq@yahoo.com>
2022-12-31 12:26:54 +1100

    Create lesson/app to use od API in react app using Axios library.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import AnimalApp from './lesson4-state/AnimalApp';
+import PicsApp from './lesson5-api/PicsApp';
 
 function App() {
   return (
     <div className='App'>
       <h1>React Version 18 Lessons</h1>
-      <AnimalApp />
+      {/* <AnimalApp /> */}
+      <PicsApp />
     </div>
   );
 }
