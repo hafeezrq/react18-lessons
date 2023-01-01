8ca428da39dc0333b7153abba0b323c445c845b8
hafeezrq <hafeezrq@yahoo.com>
2023-01-01 20:27:45 +1100

    Refactor PicsApp to use state to get images pass to ImageList comp.

--- src/lesson5-api/PicsApp.js
+++ src/lesson5-api/PicsApp.js
@@ -1,13 +1,19 @@
+import { useState } from 'react';
 import searchImages from './api';
 import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
 
 const PicsApp = () => {
-  const handleSubmit = term => {
-    searchImages(term);
+  const [images, setImages] = useState([]);
+
+  const handleSubmit = async term => {
+    const result = await searchImages(term);
+    setImages(result);
   };
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
+      <ImageList images={images} />
     </div>
   );
 };
