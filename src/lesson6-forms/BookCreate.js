75b7f864a3c01da571f456a4257b42bd69d8497b
hafeezrq <hafeezrq@yahoo.com>
2023-01-02 16:23:33 +1100

    Write BookCreate comp, send data to FormApp.

--- src/lesson6-forms/BookCreate.js
+++ src/lesson6-forms/BookCreate.js
@@ -1,5 +1,31 @@
-function BookCreate() {
-  return <div>BookCreate</div>;
+import { useState } from 'react';
+
+function BookCreate({ createBook }) {
+  const [title, setTitle] = useState('');
+  const handleChange = event => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    createBook(title);
+    setTitle('');
+  };
+  return (
+    <div className='book-create'>
+      <h3>Add a book</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor=''>Title</label>
+        <input
+          className='input'
+          type='text'
+          value={title}
+          onChange={handleChange}
+        />
+        <button className='button'>Create</button>
+      </form>
+    </div>
+  );
 }
 
 export default BookCreate;
--- src/lesson6-forms/FormsApp.js
+++ src/lesson6-forms/FormsApp.js
@@ -1,9 +1,18 @@
 import { useState } from 'react';
+import BookCreate from './BookCreate';
 
 function FormsApp() {
   const [books, setBooks] = useState([]);
 
-  return <div>FormsApp</div>;
+  const createBook = title => {
+    console.log('Add book: ', title);
+  };
+
+  return (
+    <div>
+      <BookCreate createBook={createBook} />
+    </div>
+  );
 }
 
 export default FormsApp;
