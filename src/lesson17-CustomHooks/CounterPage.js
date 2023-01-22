1fa863e53c909fc1f561aa9e8d600f00a77d535d
hafeezrq <hafeezrq@yahoo.com>
2023-01-22 21:40:31 +1100

    Refactor code to make hook function.

--- src/lesson17-CustomHooks/CounterPage.js
+++ src/lesson17-CustomHooks/CounterPage.js
@@ -2,21 +2,29 @@ import { useState, useEffect } from 'react';
 
 import Button from '../lesson10-buttons/components/Button';
 
-function CounterPage({ initialCount }) {
+function useCounter(initialCount) {
   const [counter, setCounter] = useState(initialCount);
 
   useEffect(() => {
     console.log(counter);
   }, [counter]);
 
-  const handleClick = () => {
+  const increment = () => {
     setCounter(counter + 1);
   };
 
+  return {
+    counter,
+    increment,
+  };
+}
+
+function CounterPage({ initialCount }) {
+  const { counter, increment } = useCounter(initialCount);
   return (
     <div>
       <h1>Counter: {counter}</h1>
-      <Button primary onClick={handleClick}>
+      <Button primary onClick={increment}>
         Increment Count
       </Button>
     </div>
