207a24dd0213c81410823f4af90156d9fbd11461
hafeezrq <hafeezrq@yahoo.com>
2023-01-26 10:55:43 +1100

    Introduce useReducer hook and delet useState.

--- src/lesson18-reducer/components/Counter2.js
+++ src/lesson18-reducer/components/Counter2.js
@@ -1,34 +1,43 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import { useReducer } from 'react';
 
 import Panel from '../../lesson12-props_state/components/Panel';
 import Button from '../../lesson10-buttons/components/Button';
 
-function Counter2({ initialCount }) {
-  const [counter, setCounter] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+const reducer = () => {};
 
+function Counter2({ initialCount }) {
+  // const [counter, setCounter] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+
+  // Instead we use useReducer hook.
+  const [state, dispatch] = useReducer(reducer, {
+    counter: initialCount,
+    valueToAdd: 0,
+  });
+  console.log(state);
   const increment = () => {
-    setCounter(counter + 1);
+    // setCounter(counter + 1);
   };
 
   const decrement = () => {
-    setCounter(counter - 1);
+    // setCounter(counter - 1);
   };
 
   const handleChange = event => {
     const value = parseInt(event.target.value) || 0;
 
-    setValueToAdd(value);
+    // setValueToAdd(value);
   };
 
   const handleSubmit = () => {
-    setCounter(counter + valueToAdd);
-    setValueToAdd(0);
+    // setCounter(counter + valueToAdd);
+    // setValueToAdd(0);
   };
 
   return (
     <Panel className='m-3'>
-      <h1 className='text-lg'>Counter: {counter}</h1>
+      <h1 className='text-lg'>Counter: {state.counter}</h1>
       <div className='flex flex-row space-x-4'>
         <Button primary onClick={increment}>
           Increment
@@ -42,7 +51,7 @@ function Counter2({ initialCount }) {
         <input
           className='m-3 p-1 bg-gray-50 border border-gray-300'
           type='number'
-          value={valueToAdd || ''}
+          value={state.valueToAdd || ''}
           onChange={handleChange}
         />
         <Button success>Add it</Button>
