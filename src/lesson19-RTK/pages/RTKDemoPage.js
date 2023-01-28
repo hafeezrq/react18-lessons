e3cd0368d81f412030f9e896f41976b76b5ce477
hafeezrq <hafeezrq@yahoo.com>
2023-01-28 19:34:22 +1100

    Watch for extra actions, multi states update.

--- src/lesson19-RTK/pages/RTKDemoPage.js
+++ src/lesson19-RTK/pages/RTKDemoPage.js
@@ -1,13 +1,19 @@
+import { useDispatch } from 'react-redux';
+
 import MoviePlayList from '../components/MoviePlayList';
 import SongsPlayList from '../components/SongsPlayList';
+import { reset } from '../store';
 
 import 'bulma/css/bulma.css';
+
 import '../styles.css';
 import '../store';
 
 export default function RTKDemoPage() {
+  const dispatch = useDispatch();
+
   const handleResetClick = () => {
-    //
+    dispatch(reset());
   };
 
   return (
--- src/lesson19-RTK/store/index.js
+++ src/lesson19-RTK/store/index.js
@@ -11,6 +11,9 @@ const moviesSlice = createSlice({
       const index = state.indexOf(action.payload);
       state.splice(index, 1);
     },
+    reset(state, action) {
+      return [];
+    },
   },
 });
 
@@ -26,6 +29,11 @@ const songsSlice = createSlice({
       state.splice(index, 1);
     },
   },
+  extraReducers(builder) {
+    builder.addCase('Movie/reset', (state, action) => {
+      return [];
+    });
+  },
 });
 
 const store = configureStore({
@@ -37,4 +45,4 @@ const store = configureStore({
 
 export { store };
 export const { addSong, removeSong } = songsSlice.actions;
-export const { addMovie, removeMovie } = moviesSlice.actions;
+export const { addMovie, removeMovie, reset } = moviesSlice.actions;
