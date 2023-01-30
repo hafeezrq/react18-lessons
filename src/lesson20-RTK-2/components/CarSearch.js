f54f43c02b8d9498556d75b54131827d1d3e5334
hafeezrq <hafeezrq@yahoo.com>
2023-01-30 20:36:38 +1100

    Add search functionality.

--- src/lesson20-RTK-2/components/CarSearch.js
+++ src/lesson20-RTK-2/components/CarSearch.js
@@ -1,5 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { changeSearchTerm } from '../store/slices/carsSlice';
+
 function CarSearch() {
-  return <div>CarSearch</div>;
+  const dispatch = useDispatch();
+  const searchTerm = useSelector(state => {
+    return state.cars.searhTerm;
+  });
+
+  const handleSearchTermChange = event => {
+    dispatch(changeSearchTerm(event.target.value));
+  };
+  return (
+    <div>
+      <h3>My Cars</h3>
+      <div>
+        <label htmlFor=''>Search</label>
+        <input
+          type='text'
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default CarSearch;
