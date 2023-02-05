65bc9536131e936c2366f479366bb7952302f14d
hafeezrq <hafeezrq@yahoo.com>
2023-02-05 13:29:19 +1100

    Write code to use thunks to fetch data.

--- src/lesson21-Async_Thunks/components/UserList.js
+++ src/lesson21-Async_Thunks/components/UserList.js
@@ -1,14 +1,24 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store/thunks/fetchUsers';
 
 function UserList() {
   const dispatch = useDispatch();
+  const { isLoading, data, error } = useSelector(state => {
+    return state.users;
+  });
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
-  return <div>UserList</div>;
+  if (isLoading) {
+    return <div>Loading......</div>;
+  }
+  if (error) {
+    return <div>something went wrong.</div>;
+  }
+
+  return <div>{data.length}</div>;
 }
 
 export default UserList;
