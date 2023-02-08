192e7dd39c2b37370a043580ad53b09f63eaf159
hafeezrq <hafeezrq@yahoo.com>
2023-02-08 20:31:29 +1100

    Refactor UserList and UserListItem comp.

--- src/lesson21-Async_Thunks/components/UserListItem.js
+++ src/lesson21-Async_Thunks/components/UserListItem.js
@@ -1,7 +1,22 @@
+import { GoTrashcan } from 'react-icons/go';
+import { useThunk } from '../../hooks/use-thunk';
+import Button from '../../lesson10-buttons/components/Button';
+import { removeUser } from '../store/thunks/removeUser';
+
 function UserListItem({ user }) {
+  const [doRemoveUser, isRemoving, removeUserError] = useThunk(removeUser);
+
+  const handleClick = () => {
+    doRemoveUser(user);
+  };
+
   return (
     <div className='mb-2 border rounded'>
       <div className='flex p-2 justify-betwenn items-center cursor-pointer'>
+        <Button loading={isRemoving} onClick={handleClick}>
+          <GoTrashcan />
+        </Button>
+        {removeUserError && <div>Error: In removing the user</div>}
         {user.name}
       </div>
     </div>
