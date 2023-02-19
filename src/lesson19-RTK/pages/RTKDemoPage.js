3c6501601f460d72d08df1996453a659c94daa3f
hafeezrq <hafeezrq@yahoo.com>
2023-02-19 13:05:53 +1100

    Add textual informtion about the program.

--- src/lesson19-RTK/pages/RTKDemoPage.js
+++ src/lesson19-RTK/pages/RTKDemoPage.js
@@ -18,11 +18,34 @@ export default function RTKDemoPage() {
 
   return (
     <div className='container mx-auto px-8'>
+      <header>
+        <h1 className='is-size-4 has-text-weight-medium	'>
+          Demo Features: Redux Toolkit(RTK)
+        </h1>
+        <p>
+          This program demonstrate the use of{' '}
+          <span className='has-text-info	has-text-weight-medium'>
+            Redux Toolkit (RTK)
+          </span>
+          . Concepts like{' '}
+          <span className='has-text-weight-medium'>
+            Store, Slice, action, and reducers
+          </span>
+          .
+        </p>
+        <p>
+          Code for this program is in the directory{' '}
+          <span className='is-italic'>lesson19</span>.<br /> Note: this program
+          share the store with next two following examples.
+        </p>
+      </header>
       <MoviePlayList />
       <hr />
       <SongsPlayList />
       <hr />
-      <button onClick={() => handleResetClick()} className='button is-danger'>
+      <button
+        onClick={() => handleResetClick()}
+        className='button is-danger ml-5'>
         Reset Both Playlists
       </button>
     </div>
